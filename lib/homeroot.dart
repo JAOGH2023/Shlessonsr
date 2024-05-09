66d1096558b597313b7d073dd070d0c177f49bd3
JAOGH2023 <umarovjalol27@icloud.com>
2024-05-09 19:05:37 +0500

    Appbar

---
+++ lib/homeroot.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Homeroot extends StatefulWidget {
+  const Homeroot({super.key});
+
+  @override
+  State<Homeroot> createState() => _HomerootState();
+}
+
+class _HomerootState extends State<Homeroot> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
